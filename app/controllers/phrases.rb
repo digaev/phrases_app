5fe98497c778894e25fef8dde4517507ced9a162
Nikolay Digaev <ffs.cmp@gmail.com>
2016-12-02 19:48:48 +0500

    simplify logic

--- app/controllers/phrases.rb
+++ app/controllers/phrases.rb
@@ -1,13 +1,6 @@
 PhrasesApp::App.controllers :phrases do
   get :index, map: '/', provides: [:html, :json] do
     @phrases = Phrase.limit(1).order('RANDOM()')
-    if params[:exclude].present?
-      @phrases = @phrases.where.not(
-        body: Phrase.select(:body).where(
-          id: params[:exclude].split(',')
-        ).distinct
-      )
-    end
 
     case content_type
     when :html then render 'phrases/index'
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -6,17 +6,24 @@ $(function() {
   $('#get-phrase').on('click', function(e) {
     e.preventDefault();
 
-    var ids = $('[data-phrase-id]').map(function(i, el) {
-      return $(el).attr('data-phrase-id');
-    }).get().join(',');
-
-    $.getJSON('/', { exclude: ids }, function(data) {
+    $.getJSON('/', function(data) {
       var content = $('#phrases tbody');
+      var phrases = $('tr td.phrase-body', content).map(function(i, el) {
+        return $(el).text();
+      }).get();
+
+      $.each(phrases, function(i, phrase) {
+        for (var n = data.length - 1; n >= 0; --n) {
+          if (data[n].body === phrase) {
+            data.splice(n, 1);
+          }
+        }
+      });
+
       $.each(data, function(i, obj) {
         var tr = $('<tr />');
-        tr.attr('data-phrase-id', obj.id);
         tr.append('<td>' + obj.id + '</td>');
-        tr.append('<td>' + obj.body + '</td>');
+        tr.append('<td class="phrase-body">' + obj.body + '</td>');
         tr.appendTo(content);
       });
     });
